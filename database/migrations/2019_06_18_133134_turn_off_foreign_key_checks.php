418ddae8eeb3b137746f70ed1cde123604df676d
IoanAndreiMarian <andrei.ioan@thestudio.com>
2019-06-18 13:34:54 +0300

    [master] add foreign key check migration

---
+++ database/migrations/2019_06_18_133134_turn_off_foreign_key_checks.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class TurnOffForeignKeyChecks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
